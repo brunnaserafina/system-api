4f6332c9059462bdf708740cd8ede3c57c461fe6
Brunna Serafina <brunnaserafina@gmail.com>
2023-07-07 11:07:27 -0300

    feat: implemented cpf validator

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -9,15 +9,19 @@ public class CustomerController : Controller
     [HttpPost]
     public IActionResult Register([FromBody] CustomerRequest customerRequest)
     {
-
         if (string.IsNullOrEmpty(customerRequest.name) || string.IsNullOrEmpty(customerRequest.cpf))
         {
             return BadRequest();
         }
 
+        if (!customerRequest.ValidateCPF())
+        {
+            return BadRequest("CPF inválido, falha no registro.");
+        }
+
         string messageResponse = "Pessoa cadastrada com sucesso, código "
                                     + customerRequest.cpf.Substring(0, 4);
 
-        return Json(new { status = 201, messageResponse });
+        return Json(new { status = 201, message = messageResponse });
     }
 }
--- Models/CustomerRequest.cs
+++ Models/CustomerRequest.cs
@@ -8,4 +8,59 @@ public class CustomerRequest
     public string? address { get; set; }
     public string? phone { get; set; }
 
-} 
+    public bool ValidateCPF()
+    {
+        if (string.IsNullOrEmpty(cpf))
+        {
+            return false;
+        }
+
+        string cleanedCPF = new string(cpf.Where(char.IsDigit).ToArray());
+
+        if (cleanedCPF.Length != 11)
+        {
+            return false;
+        }
+
+        if (cleanedCPF.Distinct().Count() == 1)
+        {
+            return false;
+        }
+
+        int[] multipliers1 = { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+        int[] multipliers2 = { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+
+        string cpfDigits = cleanedCPF.Substring(0, 9);
+        int sum = 0;
+
+        for (int i = 0; i < 9; i++)
+        {
+            sum += int.Parse(cpfDigits[i].ToString()) * multipliers1[i];
+        }
+
+        int remainder = sum % 11;
+        int digit1 = remainder < 2 ? 0 : 11 - remainder;
+
+        cpfDigits += digit1;
+
+        sum = 0;
+
+        for (int i = 0; i < 10; i++)
+        {
+            sum += int.Parse(cpfDigits[i].ToString()) * multipliers2[i];
+        }
+
+        remainder = sum % 11;
+        int digit2 = remainder < 2 ? 0 : 11 - remainder;
+
+        cpfDigits += digit2;
+
+        if (cleanedCPF.Substring(9) != cpfDigits.Substring(9))
+        {
+            return false;
+        }
+
+        return true;
+
+    }
+}
